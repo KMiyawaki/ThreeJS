e86c42267be7c272fc96eba4e2ef5ff6e47c341d
K.Miyawaki <5770105+KMiyawaki@users.noreply.github.com>
2020-02-20 16:28:53 +0900

    Initial commit

---
+++ js/getlibs.bat
@@ -0,0 +1,3 @@
+bitsadmin /TRANSFER job1 /PRIORITY HIGH https://raw.githubusercontent.com/mrdoob/three.js/dev/build/three.min.js %~dp0three.min.js
+bitsadmin /TRANSFER job1 /PRIORITY HIGH https://raw.githubusercontent.com/kripken/ammo.js/master/builds/ammo.js %~dp0ammo.js
+bitsadmin /TRANSFER job1 /PRIORITY HIGH https://raw.githubusercontent.com/mrdoob/three.js/dev/examples/js/controls/OrbitControls.js %~dp0OrbitControls.js
