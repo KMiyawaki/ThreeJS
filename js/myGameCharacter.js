17e3cdad8b185292260611711d0928fc8ba286ef
K.Miyawaki <5770105+KMiyawaki@users.noreply.github.com>
2020-02-23 20:40:09 +0900

    fix: method name

--- js/myGameCharacter.js
+++ js/myGameCharacter.js
@@ -48,13 +48,13 @@ class MyGameCharacter {
         this.image = null;
     }
 
-    loadImage(targetElement) {
+    show(targetElement) {
         if (this.imageURL) {
             this.image = document.createElement("img");
             let obj = this;
             this.image.onload = function () {
                 obj.onLoad();
-                obj.showImage(targetElement);
+                obj.addImage(targetElement);
             }
             this.image.src = this.imageURL;
         }
@@ -68,7 +68,7 @@ class MyGameCharacter {
         this.image.style.height = "80%";
     }
 
-    showImage(targetElement) {
+    addImage(targetElement) {
         if (this.image) {
             targetElement.appendChild(this.image);
             const targetRect = targetElement.getBoundingClientRect();
@@ -80,7 +80,7 @@ class MyGameCharacter {
         }
     }
 
-    removeImage(targetElement) {
+    remove(targetElement) {
         if (this.image) {
             targetElement.removeChild(this.image);
         }
@@ -120,7 +120,7 @@ class MyGameCharacter {
         return { hitCount: hitCount, damage: damage, message: message };
     }
 
-    show(htmlElement) {
+    print(htmlElement) {
         htmlElement.innerHTML = "";
         htmlElement.innerHTML += ("<b>" + this.name + "</b><br />");
         htmlElement.innerHTML += ("体力:" + this.hp + "<br />");
